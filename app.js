0bd4f7db33707e126aa34b12d3a42e838bf9b1d0
MASYOK <parjojuminet@gmail.com>
2026-07-16 23:34:21 +0700

    chore: update app.js entry point instructions
    Add guidance for CommonJS and ES Module configurations to support cPanel Node.js Selector.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // Startup file for cPanel Node.js Selector
 // This file points cPanel to the compiled Express server bundle
+
+// If your package.json does NOT have "type": "module" (CommonJS):
 require('./dist/server.cjs');
+
+// If your package.json HAS "type": "module" (ES Modules):
+// import './dist/server.cjs';
